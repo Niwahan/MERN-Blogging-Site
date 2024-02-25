5223c1b6ca59f8f25aeeff45d8d1454dcc7e1acc
Niwahang <aniwahang@gmail.com>
2024-02-25 14:46:45 +0545

    Added a Navigation Bar

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,15 @@
-import NavBar from "./components/navbar.component"
+import { Routes, Route } from "react-router-dom";
+import NavBar from "./components/navbar.component";
 
 const App = () => {
-    return (
-        <NavBar/>
-    )
-}
+  return (
+    <Routes>
+      <Route path="/" element={<NavBar />}>
+        <Route path="/signup" element={<h1>Sign Up Page</h1>} />
+        <Route path="/signin" element={<h1>Sign In Page</h1>} />
+      </Route>
+    </Routes>
+  );
+};
 
 export default App;
