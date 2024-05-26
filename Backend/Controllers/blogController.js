39564ac7a1e8c930b6da60bc0913f51e501a4ed8
Niwahan <aniwahang@gmail.com>
2024-05-26 20:44:50 +0545

    Created and fetched Trending Blogs

--- Backend/Controllers/blogController.js
+++ Backend/Controllers/blogController.js
@@ -23,6 +23,48 @@ export const latestBlogs = (req, res) => {
     });
 };
 
+export const trendingBlogs = (req, res) => {
+  Blog.find({ draft: false })
+    .populate(
+      "author",
+      "personal_info.profile_img personal_info.username personal_info.fullname -_id"
+    )
+    .sort({
+      "activity.total_read": -1,
+      "activity.total_likes": -1,
+      publishedAt: -1,
+    })
+    .select("blog_id title publishedAt -_id")
+    .limit(5)
+    .then((blogs) => {
+      return res.status(200).json({ blogs });
+    })
+    .catch((err) => {
+      return res.status(500).json({ error: err.message });
+    });
+};
+
+export const searchBlogs = (req, res) => {
+  let { tag } = req.body;
+  let findQuery = { tags: tag, draft: false };
+  let maxLimit = 5;
+
+  Blog.find(findQuery)
+    .populate(
+      "author",
+      "personal_info.profile_img personal_info.username personal_info.fullname -_id"
+    )
+    .sort({ publishedAt: -1 })
+    .select("blog_id title description banner activity tags publishedAt -_id")
+    .limit(maxLimit)
+    .then((blogs) => {
+      return res.status(200).json({ blogs });
+    })
+    .catch((err) => {
+      return res.status(500).json({ error: err.message });
+    });
+};
+
 export const verifyJWT = (req, res, next) => {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
