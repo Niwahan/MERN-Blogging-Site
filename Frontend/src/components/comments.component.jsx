6657288103f1235aa4d4ec083010a528b0fc6d2e
Niwahan <aniwahang@gmail.com>
2024-06-22 13:36:55 +0545

    Screen bug fixes

--- Frontend/src/components/comments.component.jsx
+++ Frontend/src/components/comments.component.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { BlogContext } from "../pages/blog.page";
 import CommentField from "./comment-field.component";
 import axios from "axios";
@@ -65,9 +65,9 @@ const CommentsContainer = () => {
   return (
     <div
       className={
-        "max-sm:w-full fixed " +
+        "fixedPosition max-sm:w-full fixed " +
         (commentsWrapper ? "top-0 sm:right-0" : "top-[100%] sm:right-[-100%]") +
-        " duration-700 max-sm:right-0 sm:top-0 w-[30%] min-w-[350px] h-full z-50 bg-white shadow-2xl p-8 px-16 overflow-y-auto overflow-x-hidden "
+        " duration-700 max-sm:right-0 sm:top-0 w-[30%] min-w-[350px] h-full z-50 bg-white shadow-2xl p-8 px-16 overflow-y-auto overflow-x-hidden"
       }
     >
       <div className="relative">
